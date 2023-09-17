24723a1fbeaf821c590e9317a8e54d3a6ec45dd3
Václav Machovský <crocoo.main@gmail.com>
2023-09-17 11:09:54 +0200

    Add project files.

---
+++ CalculatorAppUAT/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using CalculatorAppUAT.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CalculatorAppUAT.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly Calculator calc = new Calculator();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(calc);
+        }
+        
+        [HttpPost]
+        public IActionResult Index(Calculator calc)
+        {
+            if (calc.Operation == "/" && calc.SecondNumber == 0)
+                ModelState.AddModelError("zeroDivision", "Error - division with 0");
+
+            if (ModelState.IsValid)
+                calc.Count();
+
+            return View(calc);
+        }
+    }
+}
---
+++ CalculatorAppUAT/Models/Calculator.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace CalculatorAppUAT.Models
+{
+    public class Calculator
+    {
+        [Required(ErrorMessage = "Enter a number")]
+        [Display(Name = "1. number")]
+        public double FirstNumber { get; set; }
+        [Required(ErrorMessage = "Enter a number")]
+        [Display(Name = "2. number")]
+        public double SecondNumber { get; set; }
+        public double Result { get; set; }
+        public string Operation { get; set; }
+
+        public List<SelectListItem> PossibleOperations { get; set; } = new List<SelectListItem>
+        {
+            new SelectListItem { Text = "Add", Value = "+", Selected = true },
+            new SelectListItem { Text = "Subtract", Value = "-" },
+            new SelectListItem { Text = "Multiply", Value = "*" },
+            new SelectListItem { Text = "Divide", Value = "/" }
+        };
+
+        public void Count()
+        {
+            switch (Operation)
+            {
+                case "+":
+                    Result = FirstNumber + SecondNumber;
+                    break;
+                case "-":
+                    Result = FirstNumber - SecondNumber;
+                    break;
+                case "*":
+                    Result = FirstNumber * SecondNumber;
+                    break;
+                case "/":
+                    Result = FirstNumber / SecondNumber;
+                    break;
+            }
+        }
+    }
+}
